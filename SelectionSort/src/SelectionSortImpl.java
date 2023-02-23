126be1f28dfefc2cbdac07349d601d836afe22da
VIVEK KUMAR <52987349+thirumulpad@users.noreply.github.com>
2023-02-23 07:27:27 +0530

    SelectionSort Implementation

---
+++ SelectionSort/src/SelectionSortImpl.java
@@ -0,0 +1,24 @@
+public class SelectionSortImpl {
+
+    public int[] sort(int[] array){
+
+        for(int lastUnsortedIndex = array.length-1; lastUnsortedIndex>0; lastUnsortedIndex--){
+            int largest = 0;
+            for(int i = 1; i <= lastUnsortedIndex; i++){
+                if(array[i] > array[largest])
+                    largest = i;
+            }
+            swap(array, largest, lastUnsortedIndex);
+        }
+
+        return array;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        if (i == j)
+            return;
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+}
